ad773b69af9d2897105d6af68d4bdf9650da89db
vann0012 <alexandre.vannier@etudiant.univ-reims.fr>
2020-03-25 15:37:17 +0100

    Update Carte.Class + Test.Carte

---
+++ Carte.class.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types = 1);
+
+
+require_once "Couleur.class.php";
+require_once "Valeur.class.php";
+
+Class Carte
+{
+  Private $ordre; //int
+
+
+  /**
+  * Constructeur de la classe Card. Ce constructeur permet de créer
+  * une carte possedant une valeur, une couleur et un ordre.
+  * Par défaut la valeur et couleur sont des chaines vides et l'ordre est 0
+  *
+  * @param $valeur valeur de la carte
+  * @param $couleur couleur de la carte
+  * @param $ordre ordre de la carte
+  */
+
+public function __construct( String $valeur = "" , String $couleur = "", int $ordre = 0)
+{
+    $this->valeur = $valeur;
+    $this->couleur = $couleur;
+    $this->ordre = $ordre;
+}
+
+/**
+* public function __construct( Valeur $valeur = "" , Couleur $couleur = "", int $ordre = 0)
+* {
+*    $this->getValeur = $valeur;
+*    $this->getCouleur = $couleur;
+*    $this->ordre = $ordre;
+* }
+**/
+
+
+
+/**
+* Accesseur à l'ordre de la carte.  Retourne l'ordre de la carte parmi toutes les autres
+* sous forme d'une chaine de caractères.
+*
+* @return Ordre de la carte
+*/
+    public function getOrdre() : int
+    {
+        return $this->ordre;
+    }
+
+
+    public function __toString() : string
+    {
+        $res = sprintf("%9s","--------")."\n" ;
+        $res = $res ."|".sprintf("%9s","|")."\n" ;
+        $res = $res .'|'.sprintf("%6s",$this->valeur) .sprintf("%3s","|")."\n";
+        $res = $res ."|".sprintf("%9s","|")."\n" ;
+        $res = $res ."|".sprintf("%9s","|")."\n" ;
+        $res = $res ."|".sprintf("%8s",$this->couleur)."|\n" ;
+        $res = $res .sprintf("%9s","--------") ;
+        return $res;
+    }
+}
--- Test.Carte.php
+++ Test.Carte.php
@@ -1,1 +1,15 @@
-$asDeCoeur = new Card("\e[31mA\e[0m", "\e[31m:hearts:\e[0m" , 13);
+<?php
+
+declare(strict_types = 1);
+
+require_once "Carte.class.php";
+
+$asDeCoeur = new Carte("\e[31mA\e[0m", "\e[31m:hearts:\e[0m" , 13);
+echo "$asDeCoeur\n";
+$vide = new Carte;
+$asDeCarreau = new Carte("As" , "Carreau" , 13);
+echo "$asDeCarreau\n";
+$asDePique = new Carte("As" , "Pique" , 13);
+echo "$asDePique\n";
+$TroisDePique = new Carte("Trois" , "Pique" , 2);
+echo "$TroisDePique\n";
