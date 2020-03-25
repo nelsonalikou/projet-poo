30ee9c9c774decd3556b3a8c9c8385b82ef569cd
Ewan Gravé <ewan.grave@etudiant.univ-reims.fr>
2020-03-25 17:40:11 +0100

    Modification de la gestion de la couleur dans la classe Pile

--- Carte.class.php
+++ Carte.class.php
@@ -2,40 +2,39 @@
 
 declare(strict_types = 1);
 
-
 require_once "Couleur.class.php";
 require_once "Valeur.class.php";
 
 Class Carte
 {
-  Private $ordre; //int
+    Private $ordre; //int
+    private $couleur; // Couleur
+    private $valeur; // Valeur
 
+    /**
+     * Constructeur de la classe Card. Ce constructeur permet de créer
+    * une carte possedant une valeur, une couleur et un ordre.
+    * Par défaut la valeur et couleur sont des chaines vides et l'ordre est 0
+    *
+    * @param $valeur valeur de la carte
+    * @param $couleur couleur de la carte
+    * @param $ordre ordre de la carte
+    */
 
-  /**
-  * Constructeur de la classe Card. Ce constructeur permet de créer
-  * une carte possedant une valeur, une couleur et un ordre.
-  * Par défaut la valeur et couleur sont des chaines vides et l'ordre est 0
-  *
-  * @param $valeur valeur de la carte
-  * @param $couleur couleur de la carte
-  * @param $ordre ordre de la carte
-  */
+    public function __construct( String $valeur = "" , String $couleur = "", int $ordre = 0)
+    {
+        $this->valeur = $valeur;
+        $this->couleur = $couleur;
+        $this->ordre = $ordre;
+    }
 
-public function __construct( String $valeur = "" , String $couleur = "", int $ordre = 0)
-{
-    $this->valeur = $valeur;
-    $this->couleur = $couleur;
-    $this->ordre = $ordre;
-}
+    /**
+    * Accesseur à l'ordre de la carte.  Retourne l'ordre de la carte parmi toutes les autres
+    * sous forme d'une chaine de caractères.
+    *
+    * @return Ordre de la carte
+    */
 
-
-
-/**
-* Accesseur à l'ordre de la carte.  Retourne l'ordre de la carte parmi toutes les autres
-* sous forme d'une chaine de caractères.
-*
-* @return Ordre de la carte
-*/
     public function getOrdre() : int
     {
         return $this->ordre;
@@ -44,10 +43,21 @@ public function __construct( String $valeur = "" , String $couleur = "", int $or
 
     public function __toString() : string
     {
+        if ($this->couleur == "♦" || $this->couleur == "♥"){
+            $couleur = "\e[31m".$this->couleur."\e[0m";
+            $valeur = "\e[31m".$this->valeur."\e[0m";
+        }else if ($this->couleur == "♣" || $this->couleur == "♠"){
+            $couleur = "\e[30m".$this->couleur."\e[0m";
+            $valeur = "\e[30m".$this->valeur."\e[0m";
+        }else{
+            $couleur = "\e[32m".$this->couleur."\e[0m";
+            $valeur = "\e[32m".$this->valeur."\e[0m";
+        }
+
         $res = "\n" ;
-        $res = $res ." \e[47m $this->valeur\e[0m"."\e[47m   \e[0m"."\n";
+        $res = $res ." \e[47m $valeur\e[0m"."\e[47m   \e[0m"."\n";
         $res = $res ." \e[47m     \e[0m"."\n" ;
-        $res = $res ." \e[47m   $this->couleur\e[0m"."\e[47m \e[0m"."\n";
+        $res = $res ." \e[47m   $couleur\e[0m"."\e[47m \e[0m"."\n";
         return $res;
     }
 }
--- Test.Carte.php
+++ Test.Carte.php
@@ -4,5 +4,8 @@ declare(strict_types = 1);
 
 require_once "Carte.class.php";
 
-$asDeCoeur = new Carte("\e[31mA\e[0m", "\e[31m♥\e[0m" , 13);
+$asDeCoeur = new Carte("As", "♥" , 13);
+echo "$asDeCoeur\n";
+
+$asDeCoeur = new Carte("As", "♠" , 13);
 echo "$asDeCoeur\n";
