56c662a3090edcbbf5ce6bed1503cb4aa100af73
vann0012 <alexandre.vannier@etudiant.univ-reims.fr>
2020-03-26 15:11:05 +0100

    Deck.Class

--- Deck.class.php
+++ Deck.class.php
@@ -5,7 +5,28 @@ require_once("Pile.class.php");
 class Deck{
     private $cartes; // Array
 
-    public function __construct(Array $cartes){
-        
+    public function __construct(Array $cartes)
+    {
+        $this->cartes = $cartes
     }
+
+    /**
+     * La méthode piocherCarte() enlève la dernière carte du deck.
+     * Sinon, la méthode lance une exception selon l'erreur commise.
+     *
+     */
+
+    public function piocherCarte() : void
+        {
+        if ($this->getNbCartes() == 0){
+            throw new Exception("Le deck est vide");
+        }
+
+        $this->piles[$i]->retirerCarte();
+    }
+
+
+
+
+
 }
