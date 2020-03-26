5b11b0a8d713a5a71cc96743bae3abac70fed373
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2020-03-26 15:07:24 +0100

    modification du home le 26/03/2019

---
+++ Home.class.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("Pile.class.php");
+require_once("Deck.class.php");
+require_once("Table.class.php");
+
+class Home{
+    private $Home; // Pile
+
+
+    /**
+     * Constructeur de la classe Home.
+     * Prend la dernière carte de la pile et la met dans le home.
+     * 
+     * @param $Pile Pile dans laquelle l'on va prendre la carte à mettre dans le home 
+     */
+
+    public function __construct(Pile  $Pile){
+        if (count($Pile) < 1){
+            throw new Exception("Indice invalide");}
+        $this->Home[] = $this->getCarte(count($Pile)-1) ;
+    }
+
+    
+    /**
+     * La méthode ajouterCarte() ajoute la carte mise en paramètre au dessus du home.
+     * 
+     * @param $carte
+     */
+    public function ajouterCarte(Carte $carte) : void{
+        $this->Home[] = $carte;
+    }
+
+
+
+
+}
